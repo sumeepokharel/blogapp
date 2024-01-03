9134d893ca9aa4343b22660de9601003b852d42f
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2024-01-02 18:39:56 -0600

    about page and post page css

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,23 +1,31 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import { selectPosts } from "../reducers/postSlice";
 import { selectComments } from "../reducers/commentSlice";
+import styles from "./PostList.module.css";
 
 const Posts = () => {
   const posts = useSelector(selectPosts);
   const comments = useSelector(selectComments);
+
   return (
-    <div>
+    <div className={styles.postsContainer}>
       <h2>Posts</h2>
       {posts.map((post) => (
-        <div key={post.id}>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-          <p>{post.picture}</p>
-          {comments.map((comment) => (
-            <div key={comment.postId}>
-              <p>{comment.text}</p>
-            </div>
-          ))}
+        <div key={post.id} className={styles.postContainer}>
+          <div className={styles.postHeader}>
+            <h3 className={styles.postTitle}>{post.title}</h3>
+          </div>
+          <p className={styles.postContent}>{post.content}</p>
+          <div className={styles.commentContainer}>
+            {comments
+              .filter((comment) => comment.postId === post.id)
+              .map((comment) => (
+                <div key={comment.id} className={styles.comment}>
+                  <p>{comment.text}</p>
+                </div>
+              ))}
+          </div>
         </div>
       ))}
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
