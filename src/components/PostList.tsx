f82b29b715120dea57cf6159ad7b6d045a6ba116
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2024-01-02 20:52:12 -0600

    handled likereact, lovereact, deletepost, and postlist css

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,35 +1,91 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { selectPosts } from "../reducers/postSlice";
-import { selectComments } from "../reducers/commentSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectPosts,
+  loveReact,
+  likeReact,
+  deletePost,
+} from "../reducers/postSlice";
 import styles from "./PostList.module.css";
+import { FaThumbsUp, FaHeart } from "react-icons/fa";
 
-const Posts = () => {
+const PostList = () => {
+  const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
-  const comments = useSelector(selectComments);
+
+  const handleLoveReact = (postId: number) => {
+    dispatch(loveReact(postId));
+  };
+
+  const handleLikeReact = (postId: number) => {
+    dispatch(likeReact(postId));
+  };
+
+  const handleDeletePost = (postId: number) => {
+    dispatch(deletePost(postId));
+  };
 
   return (
     <div className={styles.postsContainer}>
-      <h2>Posts</h2>
+      <h2 className={styles.feedTitle}>Social Media Feed</h2>
       {posts.map((post) => (
-        <div key={post.id} className={styles.postContainer}>
+        <div key={post.id} className={styles.post}>
           <div className={styles.postHeader}>
-            <h3 className={styles.postTitle}>{post.title}</h3>
+            <h3 className={styles.postAuthor}>{post.author}</h3>
           </div>
-          <p className={styles.postContent}>{post.content}</p>
-          <div className={styles.commentContainer}>
-            {comments
-              .filter((comment) => comment.postId === post.id)
-              .map((comment) => (
-                <div key={comment.id} className={styles.comment}>
-                  <p>{comment.text}</p>
-                </div>
-              ))}
+          <p className={styles.postContent}>
+            {post.content}{" "}
+            {post.link && (
+              <a href={post.link} target="_blank" rel="noopener noreferrer">
+                Read more
+              </a>
+            )}
+          </p>
+          {post.picture && (
+            <img
+              src={post.picture}
+              alt={`Post by ${post.author}`}
+              className={styles.postImage}
+            />
+          )}
+          <div className={styles.postInfo}>
+            <p>Likes: {post.likes}</p>
+            <p>Loves: {post.loves}</p>
+            <p>Dislikes: {post.dislikes}</p>
           </div>
+          <div className={styles.postActions}>
+            <button
+              className={styles.likeButton}
+              onClick={() => handleLikeReact(post.id)}
+            >
+              <FaThumbsUp className={styles.likeIcon} />
+              Like
+            </button>
+            <button
+              className={styles.loveButton}
+              onClick={() => handleLoveReact(post.id)}
+            >
+              <FaHeart className={styles.loveIcon} />
+              Love
+            </button>
+            <button
+              className={styles.deleteButton}
+              onClick={() => handleDeletePost(post.id)}
+            >
+              Delete
+            </button>
+          </div>
+          <ul className={styles.commentsList}>
+            {post.comments.map((comment) => (
+              <li key={comment.id} className={styles.comment}>
+                {comment.text}
+              </li>
+            ))}
+          </ul>
         </div>
       ))}
     </div>
   );
 };
 
-export default Posts;
+export default PostList;
--- src/reducers/postSlice.tsx
+++ src/reducers/postSlice.tsx
@@ -1,11 +1,24 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { ChildProcess } from "child_process";
+import image from "../post-Images/floral.jpg";
+import image1 from "../post-Images/hills.png";
 
 interface Post {
   id: number;
   title: string;
   content: string;
+  author: string;
+  likes: number;
+  loves: number;
+  link?: string;
+  dislikes: number;
   picture?: string;
+  comments: Comment[];
+  date: string;
+}
+
+interface Comment {
+  id: number;
+  text: string;
 }
 
 interface PostsState {
@@ -17,14 +30,27 @@ const initialState: PostsState = {
     {
       id: 1,
       title: "Post 1",
-      content: "Content for post 1",
-      picture: "src/post-Images\floral.jpg",
+      content: "I found this interesting article about flowers",
+      author: "james Martin",
+      link: "https://linkedinheaders.com/tag/flowers/",
+      likes: 1000,
+      loves: 500,
+      dislikes: 100,
+      picture: image,
+      comments: [],
+      date: new Date().toISOString(),
     },
     {
       id: 2,
       title: "Post 2",
-      content: "Content for post 2",
-      picture: "src/post-Images\floral.jpg",
+      content: "Check out this amazing picture!",
+      author: "Robert",
+      likes: 100,
+      loves: 20,
+      dislikes: 1,
+      picture: image1,
+      comments: [],
+      date: new Date().toISOString(),
     },
   ],
 };
@@ -36,9 +62,24 @@ const postsSlice = createSlice({
     addPost: (state, action: PayloadAction<Post>) => {
       state.posts.push(action.payload);
     },
+    loveReact: (state, action: PayloadAction<number>) => {
+      const post = state.posts.find((p) => p.id === action.payload);
+      if (post) {
+        post.loves += 1;
+      }
+    },
+    likeReact: (state, action: PayloadAction<number>) => {
+      const post = state.posts.find((p) => p.id === action.payload);
+      if (post) {
+        post.likes += 1;
+      }
+    },
+    deletePost: (state, action: PayloadAction<number>) => {
+      state.posts = state.posts.filter((post) => post.id !== action.payload);
+    },
   },
 });
 
-export const { addPost } = postsSlice.actions;
+export const { addPost, loveReact, likeReact, deletePost } = postsSlice.actions;
 export const selectPosts = (state: { posts: PostsState }) => state.posts.posts;
 export default postsSlice.reducer;
